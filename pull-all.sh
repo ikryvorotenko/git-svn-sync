1e33cd486784eac55c06d2261fafc40259ffab70
merge-master <ivan.kryvorotenko@parexel.com>
2016-07-01 11:21:23 +0300

    Added gitignore, fixed current path issue

--- pull-all.sh
+++ pull-all.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-
-currentPath=$(pwd -P)
+currentPath="$(dirname "$0")"
 path="$1"
-logfile="./git-sync-log/$(date +%Y-%m-%d:%H:%M:%S)-pull.log"
+logfile="$currentPath/logs/$(date +%Y-%m-%d:%H:%M:%S)-pull.log"
+
+mkdir -p $currentPath/logs
 
 #takes all folder in provided folder and run the pull.sh against each onet-sync-log
 for folder in $path/*/ ; do
